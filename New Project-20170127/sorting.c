e20e97b1d08fed886092affa8a9bf63f24a6441d
furqanqadri <furqanqadri63@gmail.com>
2017-01-28 03:18:04 +0530

    Add files via upload

--- New Project-20170127/sorting.c
+++ New Project-20170127/sorting.c
@@ -22,3 +22,60 @@ void bubblesort()
     return;
 }
 
+void mergesort(int m_arr[],int len)
+{
+    if (len==1) return;
+    int i,size_l,size_r;
+    //break array into two halves
+    
+    size_l=len/2;
+    size_r=len-len/2;
+    int l[size_l],r[size_r];
+    
+    for(i=0;i<size_l;i++)
+        l[i]=m_arr[i];
+    
+    for(i=0;i<size_r;i++)
+        r[i]=m_arr[i+size_l];
+        
+    mergesort(l,len/2);
+    mergesort(r,len-len/2);
+    
+    merge(l,size_l,r,size_r,m_arr);
+    
+}
+
+void merge(int* l,int size_l,int* r,int size_r,int *c)
+{
+    
+    int i,j;
+    int k=0;
+    for(i=0,j=0;i<size_l && j<size_r;k++)
+    {
+        if(l[i]>r[j])
+        {
+            c[k]=r[j];
+            j++;
+        }
+        else
+        {
+            c[k]=l[i];
+            i++;
+        }
+    }
+    for(;j<size_r;k++)
+    {
+        c[k]=r[j];
+        j++;
+    }    
+    
+    for(;i<size_l;k++)
+    {
+        c[k]=l[i];
+        i++;
+    }  
+
+}
+
+
+
--- New Project-20170127/sorting.h
+++ New Project-20170127/sorting.h
@@ -1,4 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
 #define MAXSIZE 10
 
 void bubblesort(void);
 void swap(int*,int*);
+void mergesort(int*,int);
+void merge(int*,int,int*,int,int*);
