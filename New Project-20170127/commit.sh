423b0bef44494a9abb44ac9639cedebeb60fd265
furqanqadri63 <furqanqadri63@gmail.com>
2017-01-30 10:56:56 -0500

    message

--- New Project-20170127/commit.sh
+++ New Project-20170127/commit.sh
@@ -1,4 +1,4 @@
-t add -A . 
+git add -A . 
 
 
 
--- New Project-20170127/main.c
+++ New Project-20170127/main.c
@@ -4,19 +4,33 @@
 int a[MAXSIZE]={10,5,43,3423,1233,64,323,2121,6767,212};
 void display(void);
 
+#define BITS(x,p,n)\
+x^(~(~0<<(p+1)) & (~0<<(p+1-n)));
+
+#define BITS1(x,p,n)\
+x ^ ((~(~0<<n))<< p+1-n);
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c\n"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0') 
+
 int main()
 {
-    linked_list();
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  //Bit wise MACROS
+  int x=35;
+  int y=BITS1(x,3,4);
+  printf("x is %d\n",x);
+  printf("x in binary\n "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(x));
+  printf("y is %d\n",y);
+  printf("y in binary\n "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(y));
+   // linked_list();
     /*
     printf("The input array is\n");
     display();
